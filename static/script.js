ecc711696c35be715dd1f001573855cd83a0f605
richi3f <richi3f@gmail.com>
2019-12-09 02:30:57 +0100

    Upload scripts

--- static/script.js
+++ static/script.js
@@ -3,6 +3,7 @@ var curves;
 const CANDY_SIZES = [ 'XS', 'S', 'M', 'L', 'XL' ];
 const toCamel = ( str ) => str.toLowerCase().replace( /\s+([a-z])/g, ( _, chr ) => chr.toUpperCase() );
 const calcExpToLvl = ( curve, n ) => ( n <= 1 ) ? 0 : curves[ curve ]( n );
+const clamp = ( num, lb, ub ) => Math.max( lb, Math.min( num, ub ) );
 
 curves = {
     fast: function( n ) {
@@ -40,16 +41,52 @@ curves = {
 }
 
 function validate() {
-    var disable;
-    disable = !( $( 'input[name=curve]' ).val().length ) || $( 'input[name=target]' ).val() < $( 'input[name=current]' ).val();
+    var disable = false;
+    if ( !( $( 'input[name=curve]' ).val().length ) ) {
+        disable = true;
+    } else if ( parseInt( $( 'input[name=target]' ).val() ) < parseInt( $( 'input[name=current]' ).val() ) ) {
+        disable = true;
+    }
     $( '[type=submit]' ).prop( 'disabled', disable );
+}
+
+function output( value, start ) {
+    var now = new Date();
+    var d = ( now.getTime() - start.getTime() ) / 1000;
+    if ( d > 60 ) throw new Error( 'timeout' );
+    console.log( value );
+}
+
+function optimize(problem) {
+    var lp, iocp, start, colname, colval, objval;
+
+    start = new Date();
+
+    lp = glp_create_prob();
+    glp_read_lp_from_string( lp, null, problem );
+
+    glp_scale_prob( lp, GLP_SF_AUTO );
+
+    iocp = new IOCP( { presolve: GLP_ON } );
+    glp_intopt( lp, iocp );
+
+    objval = glp_mip_obj_val( lp );
+    output( 'obj: ' + objval, start );
+    for( let i = 1; i <= glp_get_num_cols( lp ); i++ ){
+        colname = glp_get_col_name( lp, i ),
+        colval = glp_mip_col_val( lp, i );
+        output( colname  + ' = ' + colval, start );
+        $( '#result-exp-candy-' + colname ).val( colval );
+        objval -= colval;
+    }
+    return objval;
 }
 
 $(document).ready(function() {
     var i, len, template, $template;
 
     // read candy MILP
-    $.get('https://raw.githubusercontent.com/richi3f/candy-calc/master/candy.txt', function(problem) {
+    $.get('https://raw.githubusercontent.com/richi3f/candy-calc/master/candy.txt', function(problemTemplate) {
 
         // read Pokémon names and experience curves
         $.getJSON( 'https://plan.pokemonteams.io/static/pokemon.json', function(pokemonData) {
@@ -94,34 +131,54 @@ $(document).ready(function() {
             $template.find( 'label' )
                 .attr( 'for', slug )
                 .text( 'Exp. Candy ' + CANDY_SIZES[i] );
-            $template.find( 'input' )
+            $template.find( 'input[type=number]' )
                 .attr( 'id', slug )
                 .attr( 'name', slug );
-            $template.appendTo( 'table tbody' );
+            $template.find( 'input[readonly]' )
+                    .attr( 'id', 'result-' + slug );
+            $template.appendTo( 'form table tbody' );
         }
+
+        // limit numerical input
+        $( 'input[type=number]' ).on( 'change', function() {
+            var value, $this;
+            value = parseInt(this.value);
+            $this = $(this);
+            if ( !isNaN(value) ) {
+                value = clamp( value, $this.attr('min'), $this.attr('max') );
+                $this.val( value );
+            } else {
+                $this.val( $this.attr('min') );
+            }
+            validate();
+        });
 
         // calculate optimal candy distribution
         $( 'form' ).on( 'submit', function( evt ) {
-            var expDiff, curve, values;
+            var expDiff, expCurrent, expTarget, curve, values, problem;
             values = {};
             evt.preventDefault();
             $.each( $( 'form' ).serializeArray(), function( _, field ) {
                 values[field.name] = field.value;
-                console.log( field.name + ' = ' + field.value )
             });
             curve = toCamel(values.curve);
-            expDiff = calcExpToLvl( curve, values.target ) - calcExpToLvl( curve, values.current );
-
+            expCurrent = calcExpToLvl( curve, values.current );
+            expTarget = calcExpToLvl( curve, values.target );
+            expDiff = expTarget - expCurrent;
+            
+            problem = problemTemplate.replace( /{exp}/g, expDiff );
             len = CANDY_SIZES.length;
             for ( i = 0; i < len; i++ ) {
                 let size = CANDY_SIZES[i].toLowerCase();
                 problem = problem.replace( RegExp( '{' + size + '}' ), values[ 'exp-candy-' + size ]);
             }
-            problem = problem.replace( /{exp}/g, expDiff );
 
-
-            alert(problem);
+            expCurrent = optimize( problem );
+            expDiff = expCurrent - expTarget;
+            console.log( 'surplus = ' + expDiff );
         });
+
+        validate();
 
     }, 'text');
 });
